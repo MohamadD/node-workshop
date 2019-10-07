cc43a420a3d5067bc16456193163122a38426001
mohamadD <rediab88@gmail.com>
2019-10-07 11:12:27 +0300

    step 4 finish

--- src/server.js
+++ src/server.js
@@ -1,21 +1,40 @@
 
 
 const http= require('http');
+var fs = require('fs');
+const port= 4000;
+
 var message = 'I am so happy to be part of the Node Girls workshop!';
 
 
 var handler= (request,response)=>{
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message); //response body
-    response.end(); // finish response
+    // var method = request.method;
+     var endpoint = request.url;
+
+    
+    if (endpoint === "/") {
+        response.writeHead(200, {"Content-Type": "text/html"});
+    
+        // const filePath = path.join(__dirname, '..', 'public', 'index.html');
+
+        fs.readFile( '../public/index.html', (error, file) =>{
+          if (error) {
+            console.log(error);
+            return;
+          }
+    
+          response.end(file);
+        });
+      }
+
 
 };
 
 
 const server=http.createServer(handler);
  
-const port= 3000;
+
 
 server.listen(port, function () {
 
